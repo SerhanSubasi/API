fa1f3656511932899b5b393cc1adab65c5c02392
Serhan Subaşı <subasiserhan@gmail.com>
2023-12-20 13:57:06 +0300

    2-6

--- src/test/java/get_requests/Get04.java
+++ src/test/java/get_requests/Get04.java
@@ -1,5 +1,6 @@
 package get_requests;
 
+import base_urls.JsonPlaceHolderBaseUrl;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -10,16 +11,12 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-public class Get04 {
+public class Get04 extends JsonPlaceHolderBaseUrl {
 
     @Test
     public void get() {
 
       // i) Url kurulacak
-        String url = "https://jsonplaceholder.typicode.com/todos/23";
-        RequestSpecification spec = new RequestSpecBuilder().setBaseUri(url).setContentType(ContentType.JSON).build();
-
-
       // ii) Beklenen data belirlenecek
       // iii) Request gönderilip Response alınacak
         given(spec).when().get().prettyPrint();
