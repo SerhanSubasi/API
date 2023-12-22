238506a449df04d155d7fb9167d11bd81ca856a9
Serhan Subaşı <subasiserhan@gmail.com>
2023-12-22 23:03:51 +0300

    4-4

---
+++ src/test/java/get_requests/Get06.java
@@ -0,0 +1,25 @@
+package get_requests;
+
+import org.junit.Test;
+
+public class Get06 {
+
+
+    @Test
+    public void get() {
+        /*
+       Given
+           https://restful-booker.herokuapp.com/booking
+       When
+           User sends get request to the URL
+       Then
+           Status code is 200
+       And
+           Among the data there should be someone whose firstname is "John" and lastname is "Smith"
+    */
+
+
+
+
+    }
+}
